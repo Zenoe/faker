b49b56a34e0021ff2844c1ffbf698f2cda4ff901
Shinigami92 <chrissi92@hotmail.de>
2022-01-11 11:43:14 +0100

    feat: migrate date

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Datatype } from './datatype';
+import { _Date } from './date';
 import { Mersenne } from './mersenne';
 import { Random } from './random';
 
@@ -151,6 +152,7 @@ export class Faker {
 
   readonly mersenne: Mersenne = new Mersenne();
   random = new Random(this);
+  readonly date: _Date = new _Date(this);
   datatype: Datatype = new Datatype(this);
 
   constructor(opts: FakerOptions = {}) {
